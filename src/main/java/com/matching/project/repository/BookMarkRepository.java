ea309cba43d33ab99e2b5e9d989025a883d0f9ea
7yhn8 <wkemrm12@naver.com>
2022-07-06 15:46:30 +0900

    feat : Add BookMarkRepository existBookMark func

--- src/main/java/com/matching/project/repository/BookMarkRepository.java
+++ src/main/java/com/matching/project/repository/BookMarkRepository.java
@@ -8,9 +8,17 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface BookMarkRepository extends JpaRepository<BookMark, Long> {
 
     @Query("select b from BookMark b join fetch b.user u join fetch b.project p where u.no = :userNo")
     public List<BookMark> findByUserNo(@Param("userNo") Long userNo);
+
+    @Query("select case when count(b) > 0 then true else false end" +
+            " from BookMark b" +
+            " inner join b.user u" +
+            " inner join b.project p" +
+            " where u.no = :userNo and p.no = :projectNo")
+    public boolean existBookMark(@Param("userNo") Long userNo, @Param("projectNo") Long projectNo);
 }
