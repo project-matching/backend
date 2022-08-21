256728c19be2c7f9e7193e3e15cb0297dd201054
dohelee <leeworld9@gmail.com>
2022-08-22 01:42:31 +0900

    feat: Add redis config

---
+++ src/main/java/com/matching/project/service/RedisService.java
@@ -0,0 +1,28 @@
+package com.matching.project.service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
+
+@Service
+@RequiredArgsConstructor
+public class RedisService {
+    private final RedisTemplate<String, Object> redisTemplate;
+    private final ObjectMapper objectMapper;
+
+    public <T> T getRedisValue(String key, Class<T> classType) throws JsonProcessingException {
+        String redisValue = (String) redisTemplate.opsForValue().get(key);
+        if (ObjectUtils.isEmpty(redisValue)) {
+            return null;
+        }else{
+            return objectMapper.readValue(redisValue, classType);
+        }
+    }
+
+    public void putRedis(String key,Object classType) throws JsonProcessingException {
+        redisTemplate.opsForValue().set(key, objectMapper.writeValueAsString(classType));
+    }
+}
